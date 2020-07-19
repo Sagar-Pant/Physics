53a0e943987e7ec06947dea8a94f92985cab9dc0
Sagar <sagarpant55@gmail.com>
2020-07-19 13:27:14 +0530

    Japanese Commit

--- src/test/java/qaClickAcademy/SeleniumTest.java
+++ src/test/java/qaClickAcademy/SeleniumTest.java
@@ -13,4 +13,9 @@ public class SeleniumTest {
     {
         System.out.println("Elements Ui");
     }
+    @Test
+    public void SeleniumTest()
+    {
+        System.out.println("Selenium Test!!!");
+    }
 }
