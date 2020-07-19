310d147859b125e927b42134ceb0d88a78b322e6
Sagar <sagarpant55@gmail.com>
2020-07-19 13:11:39 +0530

    Chinese Original Commit

---
+++ src/test/java/qaClickAcademy/PhysicsChapter.java
@@ -0,0 +1,11 @@
+package qaClickAcademy;
+
+import org.junit.Test;
+
+public class Physics1 {
+    @Test
+    public void MyPhysicsChapter()
+    {
+        System.out.println("Congo!!!This is my first physics lecture!!!");
+    }
+}
