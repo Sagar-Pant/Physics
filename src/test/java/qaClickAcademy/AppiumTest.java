90d76c1ab8b1a28909e5b07515e543b09db217d2
Sagar <sagarpant55@gmail.com>
2020-07-19 13:19:25 +0530

    Australian Commit

--- src/test/java/qaClickAcademy/AppiumTest.java
+++ src/test/java/qaClickAcademy/AppiumTest.java
@@ -1,17 +1,20 @@
 package qaClickAcademy;
-
 import org.junit.Test;
-
+import org.testng.annotations.BeforeSuite;
 public class AppiumTest {
     @Test
     public void NativeAppAndroid()
     {
         System.out.println("NativeAppAndroid");
     }
-
     @Test
     public void IOSApps()
     {
         System.out.println("IOS Apps");
+    }
+    @BeforeSuite
+    public void SafariStyle()
+    {
+        System.out.println("This is a safari style apps paradigm!!!");
     }
 }
