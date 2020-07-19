6eaf57e5ba467270964a8626eb1bff6fc5ad51ee
Sagar <sagarpant55@gmail.com>
2020-07-19 12:29:38 +0530

    Third Commit

---
+++ src/test/java/qaClickAcademy/RESTAPITest.java
@@ -0,0 +1,17 @@
+package qaClickAcademy;
+
+import org.junit.Test;
+
+public class RESTAPITest {
+    @Test
+    public void postJira()
+    {
+        System.out.println("Post Jira");
+    }
+
+    @Test
+    public void deleteTwitter()
+    {
+        System.out.println("Delete Twitter");
+    }
+}
---
+++ src/test/java/qaClickAcademy/SeleniumTest.java
@@ -0,0 +1,16 @@
+package qaClickAcademy;
+
+import org.junit.Test;
+
+public class SeleniumTest {
+    @Test
+    public void BrowserAutomation()
+    {
+        System.out.println("Browser Automation");
+    }
+    @Test
+    public void elementsUi()
+    {
+        System.out.println("Elements Ui");
+    }
+}
